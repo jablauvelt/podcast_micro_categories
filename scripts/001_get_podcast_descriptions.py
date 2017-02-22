b9e7b149da0430de24f5364d31d1850caa081f92
jablauvelt <jblauvelt8@gmail.com>
2017-02-22 00:45:45 -0800

    State as of first full run attempt

--- scripts/001_get_podcast_descriptions.py
+++ scripts/001_get_podcast_descriptions.py
@@ -4,11 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 
 import os
 import random
 import time
 import lxml.html
+import unicodedata
 
 import re
 import numpy as np
@@ -22,7 +24,7 @@ def random_sleep():
 
 # Initate driver and open Firefox window
 driver = webdriver.Firefox()
-driver.implicitly_wait(30)
+driver.implicitly_wait(15)
 driver.maximize_window()
 
 # Navigate to podcast main page
@@ -34,6 +36,12 @@ genre_names = driver.find_elements_by_xpath("//div[@id='genre-nav']//a[@class='t
 # Get list of subgenres
 sub_genre_els = driver.find_elements_by_xpath("//ul[@class='list top-level-subgenres']//a")
 sub_genre_list =  [(i.text, i.get_attribute('href')) for i in sub_genre_els]
+# Manually add subgenres that don't have subcategories
+sub_genre_list.append(['Comedy', 'https://itunes.apple.com/us/genre/podcasts-comedy/id1303?mt=2'])
+sub_genre_list.append(['Kids & Family', 'https://itunes.apple.com/us/genre/podcasts-kids-family/id1305?mt=2'])
+sub_genre_list.append(['Music', 'https://itunes.apple.com/us/genre/podcasts-music/id1310?mt=2'])
+sub_genre_list.append(['News & Politics', 'https://itunes.apple.com/us/genre/podcasts-news-politics/id1311?mt=2'])
+sub_genre_list.append(['TV & Film', 'https://itunes.apple.com/us/genre/podcasts-tv-film/id1309?mt=2'])
 
 # For each subgenre, iterate through each of the top 240 podcasts
 #FOR TESTING: ss = sub_genre_list[0]; sub_idx=0
@@ -54,25 +62,26 @@ for sub_idx, ss in enumerate(sub_genre_list):
 	podcast_els = driver.find_elements_by_xpath("//div[@id='selectedcontent']//a")
 	podcast_list =  [(i.text, i.get_attribute('href')) for i in podcast_els]
 
-	# Create list to store JSON objects
+	# Create lists to store episode JSON objects and show information, respectively
 	ep_list = []
+	show_list = []
 
 	# go through each podcast's individual page to obtain detailed show
 	# and episode information
 	#FOR TESTING: pp = podcast_list[0]; pod_idx=0
 	for pod_idx, pp in enumerate(podcast_list):
 
-		pod_name = pp[0]
+		pod_name =  unicodedata.normalize('NFKD', pp[0]).encode('ascii', 'replace').decode()
 		pod_href = pp[1]
 
 		# Print podcast parsing progress 
 		#if pod_idx % 48 == 0:
-		print("Starting %d / %d" % (pod_idx, len(podcast_list)))
+		print("Starting %d / %d: %s" % (pod_idx, len(podcast_list), pod_name))
 
 		# TEMP: only do a few per genre
-		if pod_idx > 2:
-			print("Stopping at 3 per subgenre")
-			break
+		#if pod_idx > 2:
+		#	print("Stopping at 3 per subgenre")
+		#	break
 
 		# Check for english
 		if not re.search('[a-zA-Z]', pod_name):
@@ -87,13 +96,67 @@ for sub_idx, ss in enumerate(sub_genre_list):
 		show_desc = driver.find_element_by_xpath("//div[@class='product-review']/p").text
 
 		# Do another check for UTF-8 characters
-		show_desc = unicodedata.normalize('NFKD', show_desc).encode('ascii', 'replace')
-		if re.search('\\?\\?\\?', show_desc.decode()):
+		show_desc = unicodedata.normalize('NFKD', show_desc).encode('ascii', 'replace').decode()
+		if re.search('\\?\\?\\?', show_desc):
 			print("Doesn't seem to be in english (based on show description)")
 			continue
 
 		# Get the list of script elements with the episode descriptions
 		script_els = driver.find_elements_by_xpath("//table[@class='track-list-inline-details']//script")
+
+		# Get rating and number of ratings 
+		try:
+			rating = driver.find_element_by_xpath("//div[@class='rating']/span").get_attribute('innerHTML')
+			num_ratings = driver.find_element_by_xpath("//span[@class='rating-count']").text
+			num_ratings = re.sub(' Ratings?', '', num_ratings)
+		except NoSuchElementException:
+			print("No rating for this one")
+			rating = 'N/A'
+			num_ratings = 'N/A'
+
+		# Get Author (by)
+		by = driver.find_element_by_xpath("//div[@class='left']/h2").text
+		by = re.sub('^By ', '', by)
+
+		# Get "More By ..."
+		more_by_els = driver.find_elements_by_xpath("//div[@class='extra-list more-by']//a[@class='name']")
+		more_by_vals = [i.text for i in more_by_els] 
+
+		# Get "Listeners also subscribed to ..."
+		also_sub_els = driver.find_elements_by_xpath("//div[@class='swoosh lockup-container podcast large']//a[@class='name']/span")
+		assert len(also_sub_els) == 5
+		also_sub_vals = [i.text	for i in also_sub_els]
+
+		# Get website
+		try:
+			website = driver.find_element_by_xpath("//div[@class='extra-list']/ul/li/a").get_attribute('href')
+		except NoSuchElementException:
+			print("No website for this one")
+			website = 'N/A'
+
+		# Create a dict with show-level data
+		pod_dict = {
+			'rating': rating,
+			'genre': genre,
+			'subgenre': sub_genre_name,
+			'podcast_name': pod_name,
+			'by': by,
+			'num_ratings': num_ratings,
+			'also_sub_1': also_sub_vals[0],
+			'also_sub_2': also_sub_vals[1],
+			'also_sub_3': also_sub_vals[2],
+			'also_sub_4': also_sub_vals[3],
+			'also_sub_5': also_sub_vals[4],
+			'website': website
+		}
+
+		# Add the "more by" podcasts, up to 10
+		for idx, val in enumerate(more_by_vals):
+			if idx > 10: break
+			pod_dict['more_by_' + str(idx+1)] = val
+
+		# Add the pod_dict to the show_list
+		show_list.append(pod_dict)
 
 		# Loop through script elements and extract JSON objects
 		for sc_idx, sc in enumerate(script_els):
@@ -124,8 +187,14 @@ for sub_idx, ss in enumerate(sub_genre_list):
 	print("Subgenre " + sub_genre_name + ' complete')
 
 	# Now that we've gone through all the top podcasts in the subgenre,
-	# combine them all into a pandas dataframe and export to CSV
-	df = pd.DataFrame(ep_list)
+	# combine the episode data and show-level data into two separate
+	# pandas dataframes and export to CSV
+	df_ep = pd.DataFrame(ep_list)
+	df_pod = pd.DataFrame(show_list)
 
-	df.to_csv('C:/Users/jblauvelt/Desktop/projects/podcast_micro_categories/raw/' + sub_genre_name + '.csv', index=False)
+	df_ep.to_csv('C:/Users/jblauvelt/Desktop/projects/podcast_micro_categories/raw/ep_' + sub_genre_name + '.csv', index=False)
+	df_pod.to_csv('C:/Users/jblauvelt/Desktop/projects/podcast_micro_categories/raw/pod_' + sub_genre_name + '.csv', index=False)
 
+
+# Don't forget comedy! might not have been captured
+# add podcast website
