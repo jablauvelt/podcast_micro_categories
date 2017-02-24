4157d5ab7a28d381cd94d7ce3eead5af87811e18
jablauvelt <jblauvelt8@gmail.com>
2017-02-23 23:48:03 -0600

    added comments

--- scripts/002_zip_data_files.py
+++ scripts/002_zip_data_files.py
@@ -4,14 +4,16 @@ import re
 
 os.chdir('C:/Users/jblauvelt/Desktop/projects/podcast_micro_categories/raw')
 
+# List all the files in the directory and separate zip files from data files
 files = os.listdir()
 zip_files = [i for i in files if re.search('\\.zip$', i)]
 data_files = [i for i in files if re.search('\\.csv$', i)]
 
+# Identify the subgenre for each datafile
 data_file_subgenres = [re.sub('ep\\_|pod\\_|\\.csv$', '', i) for i in data_files]
-unq_subgenres = set(data_file_subgenres)
 
-for subgenre in unq_subgenres:
+# For each subgenre, put the data files for the subgenre into their own zip
+for subgenre in set(data_file_subgenres):
 	print('Zipping ' + subgenre)
 	
 	# Check if zip file already exists, and if so, skip
@@ -19,7 +21,7 @@ for subgenre in unq_subgenres:
 		print("Zip already exists - moving on")
 		continue
 
-
+	# Create the zip
 	with zipfile.ZipFile(subgenre + '.zip', 'w', zipfile.ZIP_DEFLATED) as zz:
 		# Write all files with that subgenre (should just be two - ep_ and pod_)
 		for idx, fl in enumerate(data_files): 
